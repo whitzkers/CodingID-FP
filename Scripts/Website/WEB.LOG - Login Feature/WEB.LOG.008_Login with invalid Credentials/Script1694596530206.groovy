e2e5bbe3c4d38ea08f409074ae5bea10786136a2
BSI80196 <BSI80196@BSINB23F011.bsidomain.com>
2023-09-13 16:58:00 +0700

    importing test cases,objects,scripts,and profile for login n buy feature

---
+++ Scripts/Website/WEB.LOG - Login Feature/WEB.LOG.008_Login with invalid Credentials/Script1694596530206.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demo-app.online/')
+
+WebUI.click(findTestObject('Website/punya wiska/Homepage/btn_NavMasuk'))
+
+WebUI.setText(findTestObject('Website/punya wiska/loginPage/form_Email'), GlobalVariable.invalEmail)
+
+WebUI.setEncryptedText(findTestObject('Website/punya wiska/loginPage/form_Password'), GlobalVariable.invalPass)
+
+WebUI.click(findTestObject('Website/punya wiska/loginPage/btn_Login'))
+
+WebUI.verifyElementPresent(findTestObject('Website/punya wiska/loginPage/txt_PasswordOrEmailErrorMssg'), 0)
+
+WebUI.delay(2)
+
+WebUI.closeBrowser()
+
