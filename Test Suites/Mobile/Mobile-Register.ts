e248ff33d4f3f9751dfdafbbca1ac969b2ad3c2d
BSI80194 <BSI80194@BSINB23F009.bsidomain.com>
2023-09-14 15:42:47 +0700

    creating test suite for mobile register feature

---
+++ Test Suites/Mobile/Mobile-Register.ts
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mobile-Register</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>022a8576-b0fe-4e37-8437-7d5ff7371db4</testSuiteGuid>
+   <testCaseLink>
+      <guid>9011cb13-e14d-49d7-b909-2f9d01e2a77f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.001 - Mengakses halaman Register</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ffb9e2fe-aaea-4443-8d78-d31fb10f3ac7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.002 - Registrasi dengan input valid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6b237a22-a760-41f3-a92c-a8139bd247fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.003 - Register dengan input email invalid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>447913ba-66bc-4fbd-bc9e-63970c6f1288</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.004 - Register dengan input Whatsapp invalid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a81eccd4-b7a1-4ff1-bba9-d7a8636c19e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.005 - Register dengan Password invalid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>044a27db-72b3-4104-afa5-2bcbfb2ae7fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.006 - Register dengan input Konfirmasi Password invalid (tidak sama dengan isian pada kolom Password)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a73db07c-5c71-4d2f-bf54-22978db2d68e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.007 - Register dengan tidak menekan checkbox persetujuan persyaratan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef3f4236-f3cf-4074-8879-4c38400faa7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/MOB.REG.010 - Melakukan pendaftaran dengan menggunakan metode menggunakan Google</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>422cbd0f-f2de-4b48-ad07-55ebd4c43091</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.011 - Melakukan pengujian fitur lihat pada kolom Kata Sandi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8053e191-d749-4caf-89b5-1eeb62794a25</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.012 - Melakukan pengujian fitur lihat pada kolom Konfirmasi Kata Sandi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db701a4b-dc01-4a73-a90a-dc492b456d7d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.014 - Menguji tombol kembali pada halaman Register</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44e98763-fa51-4679-9d7f-de52fbf912cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.015 - Melakukan pendaftaran dengan input Nama kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7124db0a-88f1-408d-ac67-8d8340376cb4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.016 - Melakukan pendaftaran dengan input Tanggal Lahir kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d5ce5380-f503-4911-a6c6-7578081a0f3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.017 - Melakukan pendaftaran dengan input Email kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>06c9f4b4-2874-4761-bae6-fe094982d533</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/MOB.REG - Register Feature/WEB.REG.018 - Melakukan pendaftaran dengan input WhatsApp kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
