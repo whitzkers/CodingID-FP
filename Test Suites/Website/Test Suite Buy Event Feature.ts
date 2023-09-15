46e73d38a4d5bb028a0a4921457f6d7de4ac983c
BSI80194 <BSI80194@BSINB23F009.bsidomain.com>
2023-09-15 09:30:31 +0700

    adding test suites website buy event features

---
+++ Test Suites/Website/Test Suite Buy Event Feature.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Buy Event Feature</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>66f56d39-5cf2-4a29-99dd-dcb7dabee965</testSuiteGuid>
+   <testCaseLink>
+      <guid>5a6c6f80-af56-4082-bdb3-f300bcc3aa83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.001_accessing event page through Navbar</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1905e4f7-6c56-4f5b-a9cf-c08a76755e2c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.002_acessing detail event page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2b1e9f74-cfbe-4aa3-9a94-b4689eb2a063</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.003_add event to cart with OPEN status</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6fd1845-0e51-4cb9-9025-2c63604412ec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.003_add event to cart with unverified account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a9dc9b66-6347-4088-aaf1-e66a1ab84582</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.009_search event by event name</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>179a4f87-a2c7-4473-b1c8-2390834538fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.011_search event by facilitator</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9cf9a105-46e6-4c5a-aae5-4ea0d7fbf0fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.021_access cart page via navbar</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>82d5588e-4a00-4037-91e6-e058cc15d55e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.022_check checkbox functionality</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9252a510-319f-4cdb-8aa4-a42be2aeaf51</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.BUY - Buy Events Feature/WEB.BUY.023_accessing invoice detail page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
