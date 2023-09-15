1728d5a8a552b134bc97beb64368a4c28db268cb
BSI80194 <BSI80194@BSINB23F009.bsidomain.com>
2023-09-15 08:31:18 +0700

    adding test suites for API login TC

---
+++ Test Suites/API/Execute all Login API TC.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Execute all Login API TC</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>19cbfd07-6717-4e43-b9a0-5535644c4794</testSuiteGuid>
+   <testCaseLink>
+      <guid>91111636-3222-48ee-9436-2598040d0d3f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API.LOG - Login Feature/API.LOG.001 - Verifikasi respon untuk login berhasil</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e94315fd-7fec-45de-9900-88cab5001c71</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API.LOG - Login Feature/API.LOG.002 - Verifikasi respon untuk login gagal</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6a26170b-1446-44d7-abdb-0458b59deeb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API.LOG - Login Feature/API.LOG.003 - Verifikasi respon untuk login dengan input email kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ece63339-9512-4dc6-b0b0-a5b927e9fe3c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API.LOG - Login Feature/API.LOG.004 - Verifikasi respon untuk login dengan input password kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61e9fb8f-77be-4bdd-9033-5a887f1b2cd1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API.LOG - Login Feature/API.LOG.005 - Verifikasi respon untuk login dengan akun yang belum melakukan verifikasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
