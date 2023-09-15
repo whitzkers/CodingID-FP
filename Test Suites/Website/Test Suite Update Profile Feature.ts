7ca96547d16bf330ebe53ae0fd8394da5ee7b6e5
BSI80194 <BSI80194@BSINB23F009.bsidomain.com>
2023-09-15 09:16:14 +0700

    add test suite for Update Profile features

---
+++ Test Suites/Website/Test Suite Update Profile Feature.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Update Profile Feature</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b7d4695e-6da0-4d0c-a8cb-39fa10d276a2</testSuiteGuid>
+   <testCaseLink>
+      <guid>446bc160-89dd-40e3-995f-91c8634a8d9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.PRF - Update Profile Feature/WEB.PRF.001 - Melakukan pergantian profil (navigasi melalui Header)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b3595879-b23d-46d9-a0cd-7c49d8325dc6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.PRF - Update Profile Feature/WEB.PRF.002 - Melakukan pergantian profil (navigasi melalui URL)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1fc8177-2481-4044-9eef-7f5514f8c444</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.PRF - Update Profile Feature/WEB.PRF.003 - Melakukan pergantian profil dengan input Fullname kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc149a02-56cd-484e-9ea3-733cb9d1b25c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.PRF - Update Profile Feature/WEB.PRF.004 - Melakukan penggantian profil dengan input Phone invalid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ddc7e1b7-9bc4-4928-bf21-9e3dcfcc812c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.PRF - Update Profile Feature/WEB.PRF.008 - Menguji tombol Cancel pada halaman Edit Profil</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>87465ec3-86ef-4882-b106-219b6dcf78e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website/WEB.PRF - Update Profile Feature/WEB.PRF.010 - Melakukan pergantian profil dengan input Phone kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
