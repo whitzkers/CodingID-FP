56f7afae10571c19521b5a80524bfbab21374528
BSI80194 <BSI80194@BSINB23F009.bsidomain.com>
2023-09-18 14:29:08 +0700

    fixing API-login objects

--- Scripts/API/API.LOG - Login Feature/API.LOG.003 - Verifikasi respon untuk login dengan input email kosong/Script1694415681128.groovy
+++ Scripts/API/API.LOG - Login Feature/API.LOG.003 - Verifikasi respon untuk login dengan input email kosong/Script1694415681128.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-response = WS.sendRequest(findTestObject('API.LOG.003 - Verifikasi respon untuk login dengan input email kosong'))
+response = WS.sendRequest(findTestObject('API/API.LOG - Login Feature/API.LOG.003 - Verifikasi respon untuk login dengan input email kosong'))
 
 WS.verifyResponseStatusCode(response, 401)
 
